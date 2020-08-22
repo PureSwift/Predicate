3c0f5ad91d46930a99a62adf6878cd124393f35a
Alsey Coleman Miller <alseycmiller@gmail.com>
2020-08-21 21:45:59 -0500

    Build as dynamic library on Linux

--- Package.swift
+++ Package.swift
@@ -1,11 +1,18 @@
 // swift-tools-version:5.0
 import PackageDescription
 
+#if os(Linux)
+let libraryType: PackageDescription.Product.Library.LibraryType = .dynamic
+#else
+let libraryType: PackageDescription.Product.Library.LibraryType = .static
+#endif
+
 let package = Package(
     name: "Predicate",
     products: [
         .library(
             name: "Predicate",
+            type: libraryType,
             targets: [
                 "Predicate"
             ]
